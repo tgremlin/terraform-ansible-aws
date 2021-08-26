40c0b612b3a35315a35de91db1430f662f782630
tgremlin <allantenison@live.com>
2021-08-26 14:41:01 -0500

    trying to echo secure string to ssh  file

--- main.tf
+++ main.tf
@@ -36,11 +36,6 @@ variable "pub_sub_cidr" {
 data "aws_secretsmanager_secret" "ssh_priv" {
   arn = "arn:aws:secretsmanager:us-east-1:559481597659:secret:instance_ssh_priv-Wv8E8K"
 }
-
-variable "pvt_key" {
-  type = string
-  default = data.aws_secretsmanager_secret.ssh_priv.secret_string
-}
 resource "aws_key_pair" "instance_ssh" {
   key_name = "deployer-key"
   public_key = file(var.pub_key)
@@ -139,7 +134,7 @@ resource "aws_instance" "bastion" {
   }
 
   provisioner "remote-exec" {
-      inline = ["sudo apt update", "sudo apt install python3 -y", "echo ${pvt_key} > ${pvt_key_path}deployer.pem" ,"echo Done!"]
+      inline = ["sudo apt update", "sudo apt install python3 -y", "echo ${data.aws_secretsmanager_secret.ssh_priv.secret_string} > ${pvt_key_path}deployer.pem" ,"echo Done!"]
 
       connection {
           host          = self.public_ip
