33168f2d771618abc31da5791809c3961cd28021
tgremlin <allantenison@live.com>
2021-08-26 20:53:37 -0500

    fix path in key creation

--- vars.tf
+++ vars.tf
@@ -1,25 +1,25 @@
 variable "siteFilePath" {
     type = string
-    default = "/tmp/project/terraform-ansible-aws-main-main/ansible/site.yml"
+    default = "/files/ansible/site.yml"
 }
 variable "inventoryTemplate" {
     type = string
-    default = "/tmp/project/terraform-ansible-aws-main/ansible/staging/inventory.tmpl"
+    default = "/files/ansible/staging/inventory.tmpl"
 }
 variable "inventoryFilePath" {
     type = string
-    default = "/tmp/project/terraform-ansible-aws-main/ansible/staging/inventory"
+    default = "/files/ansible/staging/inventory"
 }
 variable "indexFilePath" {
     type = string
-    default = "/tmp/project/terraform-ansible-aws-main/ansible/roles/common/files/index.html"
+    default = "/files/ansible/roles/common/files/index.html"
 }
 variable "pub_key" {
     type = string
-    default = "/tmp/project/terraform-ansible-aws-main/ansible/roles/common/files/deployer.pub"
+    default = "/files/ansible/roles/common/files/deployer.pub"
 }
 
 variable "pvt_key" {
   type = string
-  default = "/tmp/project/terraform-ansible-aws-main/ansible/roles/common/files/deployer.pem"
+  default = "/files/ansible/roles/common/files/deployer.pem"
 }
